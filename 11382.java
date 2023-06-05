e924258ded56f2aa422796ab3ba1a549239919a0
Monswi <135089226+Monswi@users.noreply.github.com>
2023-06-05 23:42:01 +0900

    Create 11382.java

---
+++ 11382.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		long a = scanner.nextLong();
+		long b = scanner.nextLong();
+		long c = scanner.nextLong();
+		long sum = a+b+c;
+		System.out.println(sum);
+		
+	}
+}
