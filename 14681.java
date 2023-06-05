d1e9e4617a231c5708ba9c986f3e7adb8532c890
Monswi <135089226+Monswi@users.noreply.github.com>
2023-06-06 00:47:34 +0900

    Create 14681.java

---
+++ 14681.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a, b;
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		
+		if (a>0 && b> 0) {
+			System.out.println("1");
+		}else if (a>0 && b< 0) {
+			System.out.println("4");
+		} else if (a<0 && b> 0) {
+			System.out.println("2");
+		} else {
+			System.out.println("3");
+		}
+	}
+}
