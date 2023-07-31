a4bcc394beb83ff8c2477f606f5240c26ba2c120
Monswi <135089226+Monswi@users.noreply.github.com>
2023-08-01 01:21:37 +0900

    Update 14681.java

--- 14681.java
+++ 14681.java
@@ -8,7 +8,7 @@ public class Main {
 		
 		if (a>0 && b> 0) {
 			System.out.println("1");
-		}else if (a>0 && b< 0) {
+		} else if (a>0 && b< 0) {
 			System.out.println("4");
 		} else if (a<0 && b> 0) {
 			System.out.println("2");
