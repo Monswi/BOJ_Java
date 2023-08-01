18452db2b1144102e88821bcaad37faf6c1d1635
Monswi <135089226+Monswi@users.noreply.github.com>
2023-08-01 23:51:58 +0900

    Update 2754.java

--- 2754.java
+++ 2754.java
@@ -1,52 +1,31 @@
 import java.util.Scanner;
-//2754/틀려서 다시 해야 함
+
 public class Main {
    public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
       String x = sc.next();
       String y = x.substring(0, 1);
-      String z = x.substring(1, 2);
-
-      double a = 4.0;
-      double b = 3.0;
-      double c = 2.0;
-      double d = 1.0;
-      double f = 0.0;
-
-      if (y.equals("A")) {
-         if (z.equals("0")) {
-            System.out.println(a);
-         } else if (z.equals("+")) {
-            System.out.print(a + 0.3);
-         } else {
-            System.out.print(a - 0.3);
-         }
-      } else if (y.equals("B")) {
-         if (z.equals("0")) {
-            System.out.println(b);
-         } else if (z.equals("+")) {
-            System.out.print(b + 0.3);
-         } else {
-            System.out.print(b - 0.3);
-         }
-      } else if (y.equals("C")) {
-         if (z.equals("0")) {
-            System.out.println(c);
-         } else if (z.equals("+")) {
-            System.out.print(c + 0.3);
-         } else {
-            System.out.print(c - 0.3);
-         }
-      } else if (y.equals("D")) {
-         if (z.equals("0")) {
-            System.out.println(d);
-         } else if (z.equals("+")) {
-            System.out.print(d + 0.3);
-         } else {
-            System.out.print(d - 0.3);
-         }
-      } else if (y.equals("F")) {
-         System.out.print(f);
+      double score = 0.0;
+      
+      switch(y) {
+      	case "A": score = 4.0;break;
+      	case "B": score = 3.0;break;
+      	case "C": score = 2.0;break;
+      	case "D": score = 1.0;break;
+      	case "F": score = 0.0;break;
+      }
+      
+      if(x.length()>1) {
+    	  String z = x.substring(1, 2);
+    	  if(z.equals("+")) {
+    		  System.out.println(score+0.3);
+    	  } else if(z.equals("-")) {
+    		  System.out.println(score-0.3);
+    	  } else {
+    		  System.out.println(score);
+    	  }
+      } else {
+      System.out.println(score);
       }
    }
 }
