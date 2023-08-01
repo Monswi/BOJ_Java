2465e5b0446c43ab50d3fcac25d50a68dbc40b99
Monswi <135089226+Monswi@users.noreply.github.com>
2023-08-02 01:15:22 +0900

    Create 2439.java

---
+++ 2439.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		char s = '*';
+		char b = ' ';
+		for(int i=1;i<=a;i++) {
+			for(int e=1;e<=a-i;e++) {
+				System.out.print(b);
+			} 
+			for(int e=1;e<=i;e++) {
+				System.out.print(s);
+			} System.out.println();
+		}
+	}
+}
