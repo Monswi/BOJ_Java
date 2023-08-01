88997bae8482b1aae5d6c5a675c485ca81a96644
Monswi <135089226+Monswi@users.noreply.github.com>
2023-08-02 01:28:08 +0900

    Create 2441.java

---
+++ 2441.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		char s = '*';
+		char b = ' ';
+		
+		for(int i=0;i<a;i++) {
+			for(int e=0;e<i;e++) {
+				System.out.print(b);
+			} for(int e=0;e<5-i;e++) {
+				System.out.print(s);
+			} System.out.println();
+		}
+	}
+}
