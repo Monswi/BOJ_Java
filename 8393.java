451784e65ae9956193d9f8250f4aeed30659ebb8
Monswi <135089226+Monswi@users.noreply.github.com>
2023-08-05 14:09:18 +0900

    Update 8393.java

--- 8393.java
+++ 8393.java
@@ -4,7 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		System.out.println((n * (n + 1)) / 2);
-
+		int a = 0;
+		for (int i = 1; i <= n; i++) {
+			a += i;
+		}
+		System.out.println(a);
 	}
 }
