456abb6df56acc2cbd5482a3b25bee07924a8d2d
Monswi <135089226+Monswi@users.noreply.github.com>
2023-08-08 00:17:35 +0900

    Create 2884.java

---
+++ 2884.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		
+		if (m>=45) {
+			System.out.println(h);
+			System.out.println(m-45);
+		} else {
+			if(h>0) {
+				System.out.println(h-1);
+				System.out.println(m+15);
+			} else {
+				System.out.println(23);
+				System.out.println(m+15);
+			}
+		}
+	}
+}
