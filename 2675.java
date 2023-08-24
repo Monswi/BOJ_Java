bab521ccf3a55ce5fb59cd614d4e0b1c1793e592
Monswi <135089226+Monswi@users.noreply.github.com>
2023-08-25 00:07:07 +0900

    Create 2675.java

---
+++ 2675.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int test = sc.nextInt();
+		for(int i=0;i<test;i++) {
+			int num = sc.nextInt();
+			String s = sc.next();
+			for(int j=0;j<s.length();j++) {
+				for(int k=0;k<num;k++) {
+					System.out.print(s.charAt(j));
+				}
+			}
+			System.out.println("");
+		}
+	}
+}
