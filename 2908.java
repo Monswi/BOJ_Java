f38e4abfbfff0428150f774e25d2ed921a471302
Monswi <135089226+Monswi@users.noreply.github.com>
2023-08-28 23:59:37 +0900

    Update 2908.java

--- 2908.java
+++ 2908.java
@@ -7,22 +7,10 @@ public class Main{
 		int re_a = (a%10)*100+((a/10)%10)*10+(a/100);
 		int re_b = (b%10)*100+((b/10)%10)*10+(b/100);
 		
-		if(a%10>b%10) {
+		if(re_a>re_b) {
 			System.out.println(re_a);
-		} else if(b%10>a%10) {
+		}else {
 			System.out.println(re_b);
-		} else {
-			if((a/10)%10>(b/10)%10) {
-				System.out.println(re_a);
-			}else if((b/10)%10>(a/10)%10) {
-				System.out.println(re_b);
-			} else {
-				if((a/100)>(b/100)) {
-					System.out.println(re_a);
-				}else{
-					System.out.println(re_b);
-				} 
-			}
 		}
 	}
 }
