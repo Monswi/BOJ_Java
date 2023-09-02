280a4737422be4479dba3813b977a7746c3912ab
Monswi <135089226+Monswi@users.noreply.github.com>
2023-09-02 13:22:22 +0900

    Update and rename 1978.java to *1978.java

--- *1978.java
+++ *1978.java
@@ -1,4 +1,43 @@
 import java.util.Scanner;
+//수정후
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int count = 0;
+
+		for (int i = 0; i < n; i++) {
+			int num = sc.nextInt();
+
+			if (isPrime(num) == true) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+
+	public static boolean isPrime(int number) {
+		if (number == 1)
+			return false;
+
+		if (number == 2)
+			return true;
+		if (number % 2 == 0)
+			return false;
+
+		for (int i = 3; i * i <= number; i += 2) {
+			if (number % i == 0)
+				return false;
+		}
+		return true;
+	}
+}
+
+
+
+/////수정전
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
