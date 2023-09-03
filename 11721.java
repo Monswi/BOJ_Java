186dbd57e5c5ea34a0bbc1ce44d4b2c9d1103374
Monswi <135089226+Monswi@users.noreply.github.com>
2023-09-03 13:49:24 +0900

    Update 11721.java

--- 11721.java
+++ 11721.java
@@ -4,17 +4,12 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String word = sc.next();
-
+		
+		for (int i = 0; i < word.length() / 10; i++) {
+			System.out.println(word.substring(10 * i, 10 * (i + 1)));
+		}
 		if (word.length() % 10 != 0) {
-			for (int i = 0; i < word.length() / 10; i++) {
-				System.out.println(word.substring(10 * i, 10 * (i + 1)));
-			}
 			System.out.println(word.substring(10 * (word.length() / 10), word.length()));
-		} else {
-			for (int i = 0; i < (word.length() / 10) - 1; i++) {
-				System.out.println(word.substring(10 * i, 10 * (i + 1)));
-			}
-			System.out.println(word.substring(10 * ((word.length() / 10) - 1), word.length()));
 		}
 	}
 }
